6816c5aff49fe314a63704010ca6b9a9a6137309
Michael Crismali <michael.crismali@gmail.com>
2013-02-15 17:37:58 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TYML::Application.config.secret_token = '834dcb03bd25c07a8379044224fd7b41214f791823c891ddebd96e0b833321c029759c870d1e7c646099a2ac25dc8e383602e3b4aa38d6235f0b2418d248cf28'
