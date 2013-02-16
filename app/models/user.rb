8e4644f0bf5cd35537846e4418c048685ca32c07
Michael Crismali <michael.crismali@gmail.com>
2013-02-15 19:14:57 -0600

    setup users table relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :bio, :email, :location, :name, :profile_pic
 
-  has_many :tymls
+  has_many :sent_tymls, class_name: 'Tyml', foreign_key: 'sender_id'
+  has_many :received_tymls, class_name: 'Tyml', foreign_key: 'receiver_id'
 
 end
