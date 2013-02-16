cc70a3cba3a2af44a759e4fdf98e4ca42c6d2bbf
Michael Crismali <michael.crismali@gmail.com>
2013-02-15 18:34:55 -0600

    wrote seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Tyml.destroy_all
+
+users = [
+  {name: 'Mike McGee', email: 'mike@starterleague.com', profile_pic: 'http://assets2.starterleague.com/assets/mike-mcgee-16075ffb857b541ef073ddb483c9bca6.jpg', bio: 'I did cool things', location: 'Chicago, IL' },
+  {name: 'Michael Crismali', email: 'michael.crismali@gmail.com', profile_pic: 'http://portal.starterleague.com/assets/michael-crismali-01525144b8bc333ee687fbb51832f2ca.jpg', bio: 'I went to a hippy school', location: 'Chicago, IL' },
+  {name: 'Natasha Postolovski', email: 'npostolovski@gmail.com', profile_pic: 'http://portal.starterleague.com/assets/natasha-postolovski-709bd16708b02bffca8a35e9c75ac809.jpg', bio: 'I did even cooler things', location: 'Melbourne, AU' },
+  {name: 'Peter Ostrander', email: 'peter.ostrander@gmail.com', profile_pic: 'http://portal.starterleague.com/assets/peter-ostrander-33542548f4158794e11eb689e08eec6c.jpg', bio: 'I have to go to the airport', location: 'Chicago, IL' },
+  {name: 'Elizabeth Park', email: 'elibeee@gmail.com', profile_pic: 'http://portal.starterleague.com/assets/elizabeth-park-42c231497c42a6bf78202b8541188f9b.jpg', bio: 'I like mockups', location: 'Chicago, IL' },
+  {name: 'Archie Algas', email: 'archie.algas@gmail.com', profile_pic: 'http://portal.starterleague.com/assets/archie-algas-8cb8248639404527654c53b61a1df0e5.jpg', bio: 'I like mockups even more', location: 'Chicago, IL' }
+]
+
+User.create users
+
+notes = [ "one kind of note", "different note", "yet another note", "even more notes"]
+urls = [ 'http://www.yelp.com/', 'https://www.google.com/', 'https://twitter.com/', 'http://wikipedia.org/', 'https://twitter.com/jeffcohen']
+
+
+100.times do
+  tyml = Tyml.new
+  tyml.note = notes[rand(4)]
+  tyml.url = urls[rand(5)]
+  tyml.sender_id = User.offset(rand(6)).first.id
+  tyml.receiver_id = User.offset(rand(6)).first.id
+
+  tyml.save
+end
