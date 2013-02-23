476c7c814ed1c968ecc0178ea329e516f517664d
Michael Crismali <michael.crismali@gmail.com>
2013-02-23 11:56:11 -0600

    validates uniqueness and presence of user emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :bio, :email, :location, :name, :profile_pic
 
+  validates_presence_of :email
+  validates_uniqueness_of :email
+
   has_many :sent_tymls, class_name: 'Tyml', foreign_key: 'sender_id'
   has_many :received_tymls, class_name: 'Tyml', foreign_key: 'receiver_id'
 
