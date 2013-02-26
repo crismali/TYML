69ac2e0a114d5c2aca23bb2d1bcf8f080ddedbb2
Natasha Postolovski <ledriver@gmail.com>
2013-02-26 15:53:26 -0600

    More mailer shizzzzz

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TYML::Application.initialize!
+
+# Configuration for using SendGrid on Heroku
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "npostolovski",
+  :password => "F3Rw$mjL*uAu",
+  :domain => "floating",
+  :address => "http://floating-springs-9521.herokuapp.com/",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
