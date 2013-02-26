81246ff8adf441268e12ff169a235bd10c7e0fbb
Natasha Postolovski <ledriver@gmail.com>
2013-02-26 16:21:53 -0600

    OK, it's SendGrid sending mail.

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,13 @@ require File.expand_path('../application', __FILE__)
 TYML::Application.initialize!
 
 # Configuration for using SendGrid on Heroku
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.smtp_settings = {
-#   :user_name => "npostolovski",
-#   :password => "F3Rw$mjL*uAu",
-#   :domain => "floating-springs-9521.herokuapp.com",
-#   :address => "smtp.sendgrid.net",
-#   :port => 587,
-#   :authentication => :plain,
-#   :enable_starttls_auto => true
-# }
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "npostolovski",
+  :password => "F3Rw$mjL*uAu",
+  :domain => "floating-springs-9521.herokuapp.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
