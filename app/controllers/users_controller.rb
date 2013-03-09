cf375bd693228c08c02086cd07972e4d85359e1e
Natasha Postolovski <ledriver@gmail.com>
2013-03-09 12:05:50 -0600

    Can update user details via your Settings page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    @current_user = current_user
     @tyml = Tyml.new
     respond_to do |format|
       format.html # show.html.erb
